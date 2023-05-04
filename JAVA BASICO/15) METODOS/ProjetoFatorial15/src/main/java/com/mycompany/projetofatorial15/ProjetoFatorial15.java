12d730ca8e40dd15f394482fb5bf2d4363416fca
CODIGOS™ <107000099+VILHALVA@users.noreply.github.com>
2023-05-04 16:41:01 -0500

    Add files via upload

---
+++ JAVA BASICO/15) METODOS/ProjetoFatorial15/src/main/java/com/mycompany/projetofatorial15/ProjetoFatorial15.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetofatorial15;
+
+public class ProjetoFatorial15 {
+    public static void main(String[] args) {
+        Fatorial f = new Fatorial();
+    }
+}
