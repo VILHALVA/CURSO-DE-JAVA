14159ba339ccce3797291136fb2f2182775a5a58
CODIGOS™ <107000099+VILHALVA@users.noreply.github.com>
2023-05-04 16:40:22 -0500

    Add files via upload

---
+++ JAVA BASICO/14) VETORES/Vetores01/src/main/java/com/mycompany/vetores01/Vetores04.java
@@ -0,0 +1,13 @@
+package com.mycompany.vetores01;
+
+import java.util.Arrays;
+
+public class Vetores04 {
+    public static void main(String[] args) {
+        int v[] = new int[20];
+        Arrays.fill(v, 0);
+        for (int valor: v) {
+            System.out.print(valor + " ");
+        }
+    }
+}
